686b81bb5e5d74b75c4738758684f123a83bb402
CaesiumY <mn0658@naver.com>
2019-10-29 17:36:25 +0900

    feat: Change pwa manifest options

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   pwa: {
-    themeColor: "#ff9e1b"
+    manifestOptions: {
+      gcm_sender_id: "000",
+      background_color: "#ff9e1b"
+    },
+    themeColor: "#000000"
   }
 };
