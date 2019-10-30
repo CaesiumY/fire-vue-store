8b46bd71aa1c7af17097bad88138fb881f917ad0
CaesiumY <mn0658@naver.com>
2019-10-30 12:54:47 +0900

    feat: Add firebase module

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import * as firebase from "firebase";
+import token from "../tokens.json";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: token.firebaseConfig.apiKey,
+  authDomain: token.firebaseConfig.authDomain,
+  databaseURL: token.firebaseConfig.databaseURL,
+  projectId: token.firebaseConfig.projectId,
+  storageBucket: token.firebaseConfig.storageBucket,
+  messagingSenderId: token.firebaseConfig.messagingSenderId,
+  appId: token.firebaseConfig.appId
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
