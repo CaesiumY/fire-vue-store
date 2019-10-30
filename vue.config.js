c799584e1fdd251a2809d1e945ac3856b2cf9402
CaesiumY <mn0658@naver.com>
2019-10-30 11:10:22 +0900

    chore: Add gcm sender id

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pwa: {
     manifestOptions: {
-      gcm_sender_id: "000",
+      gcm_sender_id: require("./tokens.json").gcm_sender_id,
       background_color: "#ff9e1b"
     },
     themeColor: "#000000"
