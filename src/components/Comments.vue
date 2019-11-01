7e643144f419f24f362fcbe5fcc1b6a28ab8ceb5
CaesiumY <mn0658@naver.com>
2019-11-01 17:19:41 +0900

    style: Change Comments component style

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -7,7 +7,9 @@
       <div class="reviews" v-for="review in reviews" :key="review.id">
         <h3 class="reviewTitle">{{review.title}}</h3>
         <p class="reviewBody">{{review.body}}</p>
-        <span class="reviewTag" v-for="(tag, index) in review.tags" :key="index">#{{tag}}</span>
+        <div class="reviewTags">
+          <span class="reviewTag" v-for="(tag, index) in review.tags" :key="index">#{{tag}}</span>
+        </div>
       </div>
     </main>
   </div>
@@ -43,7 +45,28 @@ export default {
 </script>
 
 <style scoped>
+.reviewMain {
+  display: flex;
+  flex-direction: column;
+}
 .reviews {
-  margin-top: 55px;
+  border: 3px solid #e55039;
+  border-radius: 8px;
+  box-shadow: 1px 1px 1px #ccc;
+  margin: 15px 0;
+  cursor: pointer;
+}
+
+.reviewBody {
+  margin: 10px;
+}
+
+.reviewTags {
+  margin-bottom: 10px;
+}
+
+.reviewTag {
+  margin: 5px;
+  color: #3498db;
 }
 </style>
